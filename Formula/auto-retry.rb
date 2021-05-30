8d65181e9183653f5eebd7ce9ec627ab724499a7
Jose Diaz-Gonzalez <email@josediazgonzalez.com>
2021-05-29 23:47:13 -0400

    chore: bump formula

--- Formula/auto-retry.rb
+++ Formula/auto-retry.rb
@@ -1,7 +1,7 @@
 class AutoRetry < Formula
   desc "Automatically retries - with exponential backoff - a given command until it exits 0"
   homepage "http://josediazgonzalez.com"
-  url "https://raw.githubusercontent.com/josegonzalez/homebrew-repo/main/bin/auto-retry"
+  url "https://raw.githubusercontent.com/josegonzalez/homebrew-repo/51ecea74196712d205ca516180fa2179a982bcbc/bin/auto-retry"
   sha256 "7492c16ee02ef64d6856e633a141cd9a9687ecd6641495cca54470eba825dfe4"
   version "0.1.0"
 
