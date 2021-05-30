a592e10c1e61c7f0a031a0e8e5eaca7da6d04192
Jose Diaz-Gonzalez <email@josediazgonzalez.com>
2021-05-29 23:35:16 -0400

    fix: add missing sha256 sum

--- Formula/generate-help-markdown.rb
+++ Formula/generate-help-markdown.rb
@@ -2,6 +2,7 @@ class GenerateHelpMarkdown < Formula
   desc "Uses reflection on scripts to show computed help output"
   homepage "http://josediazgonzalez.com"
   url "https://raw.githubusercontent.com/josegonzalez/homebrew-repo/main/bin/generate-help-markdown"
+  sha256 "6cfc349fd9540fd4d106ee68a572536a10295da0445f86e68995a9659815f0e8"
 
   def install
     bin.install "generate-help-markdown"
