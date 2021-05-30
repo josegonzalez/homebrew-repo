4b006203dd6e15aa14b3d93114d7179029c9624a
Jose Diaz-Gonzalez <email@josediazgonzalez.com>
2021-05-29 23:37:35 -0400

    fix: set version that cannot be computed

--- Formula/generate-help-markdown.rb
+++ Formula/generate-help-markdown.rb
@@ -3,6 +3,7 @@ class GenerateHelpMarkdown < Formula
   homepage "http://josediazgonzalez.com"
   url "https://raw.githubusercontent.com/josegonzalez/homebrew-repo/main/bin/generate-help-markdown"
   sha256 "6cfc349fd9540fd4d106ee68a572536a10295da0445f86e68995a9659815f0e8"
+  version "0.1.0"
 
   def install
     bin.install "generate-help-markdown"
