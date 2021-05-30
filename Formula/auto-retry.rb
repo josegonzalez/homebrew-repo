51ecea74196712d205ca516180fa2179a982bcbc
Jose Diaz-Gonzalez <email@josediazgonzalez.com>
2021-05-29 23:42:35 -0400

    fix: correct sha

--- Formula/auto-retry.rb
+++ Formula/auto-retry.rb
@@ -2,7 +2,7 @@ class AutoRetry < Formula
   desc "Automatically retries - with exponential backoff - a given command until it exits 0"
   homepage "http://josediazgonzalez.com"
   url "https://raw.githubusercontent.com/josegonzalez/homebrew-repo/main/bin/auto-retry"
-  sha256 "45f476a147d8d127a4ca28a7071e581d4ce140eb062ccd108046a58e94a6e7ab"
+  sha256 "7492c16ee02ef64d6856e633a141cd9a9687ecd6641495cca54470eba825dfe4"
   version "0.1.0"
 
   def install
