ec1374e701e795f43a5ac4cfce8852834cfd1b1d
Jose Diaz-Gonzalez <email@josediazgonzalez.com>
2021-09-25 17:24:24 -0400

    fix: update auto-retry

--- Formula/auto-retry.rb
+++ Formula/auto-retry.rb
@@ -1,8 +1,8 @@
 class AutoRetry < Formula
   desc "Automatically retries - with exponential backoff - a given command until it exits 0"
   homepage "http://josediazgonzalez.com"
-  url "https://raw.githubusercontent.com/josegonzalez/homebrew-repo/ab71713d4da602f98470cb0e10bc59a21a88d8a0/bin/auto-retry"
-  sha256 "8a3546eddec7d001f6419cf875a9bcc3f526ba1805f18500f250c375bdd92a69"
+  url "https://raw.githubusercontent.com/josegonzalez/homebrew-repo/51a8c6e1374738deb7218defe15ee3e254f45d6c/bin/auto-retry"
+  sha256 "4094b029612bef01f608dfce824e09278e0e8a3cf191a18bb220df6d26175471"
   version "1.0.0"
 
   def install
