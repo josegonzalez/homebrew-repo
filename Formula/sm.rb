e0bb0509dfd2a21e4c8cf78d3a59e1e6b63fdef1
Jose Diaz-Gonzalez <email@josediazgonzalez.com>
2024-08-14 07:36:30 -0400

    chore: update sm formula

--- Formula/sm.rb
+++ Formula/sm.rb
@@ -1,8 +1,8 @@
 class Sm < Formula
   desc "Simple secret management tool for server configuration"
   homepage "https://github.com/josegonzalez/sm"
-  url "https://github.com/josegonzalez/sm/archive/refs/tags/v0.12.0.tar.gz"
-  sha256 "7146b8cf14b4e031ef763d8007c00df201f9ef9667b985ebbe3c71fdb5112abb"
+  url "https://github.com/josegonzalez/sm/archive/refs/tags/v0.13.0.tar.gz"
+  sha256 "405c069b016f9fcc99b3cdcd2eda0054098d8180da2a6d562adf1a0bdb49143e"
   license "MIT"
   head "https://github.com/josegonzalez/sm.git", branch: "master"
 
